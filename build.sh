c191cbbc5cbe913be23cc9f5307ecf034b6e2b3a
kpattar <kpattar@altimetrik.com>
2025-07-14 10:24:44 +0530

    corrected bash commands

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
-# build_files.sh
+#!/bin/bash
+set -e  # Stop script if any command fails
+
+# Install dependencies
 pip install -r requirements.txt
 
-# make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+# Run migrations
+python3.9 manage.py migrate
+
+# Collect static files without prompts
+python3.9 manage.py collectstatic --noinput
+
